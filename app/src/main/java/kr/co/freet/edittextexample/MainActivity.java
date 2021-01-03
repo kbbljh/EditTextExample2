8eb6dcd25476ced91c06688e1e6e2df4c647fdee
Byung Bang Kim <bbkim73@gmail.com>
2021-01-03 23:28:13 +0900

    My First Commit

---
+++ app/src/main/java/kr/co/freet/edittextexample/MainActivity.java
@@ -0,0 +1,73 @@
+package kr.co.freet.edittextexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonConfirm;
+
+    String usernameInput;
+    String passwordInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextUsername = findViewById(R.id.edit_text_username);
+        editTextPassword = findViewById(R.id.edit_text_password);
+        buttonConfirm = findViewById(R.id.button_confirm);
+
+        editTextUsername.setOnEditorActionListener(editorListener);
+        editTextPassword.setOnEditorActionListener(editorListener);
+
+        editTextUsername.addTextChangedListener(loginTextWatcher);
+        editTextPassword.addTextChangedListener(loginTextWatcher);
+
+    }
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            usernameInput = editTextUsername.getText().toString().trim();
+            passwordInput = editTextPassword.getText().toString().trim();
+
+            buttonConfirm.setEnabled(!usernameInput.isEmpty() && !passwordInput.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
+    private TextView.OnEditorActionListener editorListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+            switch (i) {
+                case EditorInfo.IME_ACTION_NEXT:
+                    Toast.makeText(MainActivity.this, "Next", Toast.LENGTH_SHORT).show();
+                    break;
+                case EditorInfo.IME_ACTION_SEND:
+                    Toast.makeText(MainActivity.this, "Send", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return false;
+        }
+    };
+}
