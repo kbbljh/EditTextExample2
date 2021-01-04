38239d2b48a38bde1fe115146d909f6fd4255842
dk0418 <dk0418@nate.com>
2021-01-04 20:40:06 +0900

    git push test3

--- app/src/main/java/kr/co/freet/edittextexample/MainActivity.java
+++ app/src/main/java/kr/co/freet/edittextexample/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     String passwordInput;
 
     @Override
-    // git push test2
+    // git push test3
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
